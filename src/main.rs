d01e028556f6f6b56c84b583241a96d5b44092b6
Mario Tonin <mario.tonin.59@gmail.com>
2023-08-20 17:59:18 +0200

    minor edit before optimisation of the hit_sphere

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,12 @@ mod ray;
 fn ray_color(r: Ray) -> Vec3 {
     let t: f64 = hit_sphere(Vec3::new(0.0, 0.0, -1.0), 0.5, &r);
     if t > 0.0 {
-        let N = unit_vector(r.at(t) - Vec3::new(0.0, 0.0, -1.0));
-        return 0.5 * Vec3::new(N.x()+1.0, N.y()+1.0, N.z()+1.0);
+        let n: Vec3 = unit_vector(r.at(t) - Vec3::new(0.0, 0.0, -1.0));
+        return 0.5 * Vec3::new(n.x()+1.0, n.y()+1.0, n.z()+1.0);
     }
 
     let unit_direction: Vec3 = unit_vector(r.direction());
-    let a = 0.5 * (unit_direction.y() + 1.0);
+    let a: f64 = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - a) * Vec3::new(1.0, 1.0, 1.0) + a * Vec3::new(0.5, 0.7, 1.0);
 }
 
